64348e30a42955a61ae7d40b2f70dcbf95e35b4e
Tvisha <shahtvisha03@gmail.com>
2026-08-14 16:48:31 -0700

    Set xArm7 initial pose to j6=-90, j7=20

--- dimos/hardware/manipulators/xarm/adapter.py
+++ dimos/hardware/manipulators/xarm/adapter.py
@@ -41,8 +41,7 @@ MAX_CARTESIAN_SPEED_MM = 500.0  # Max cartesian speed in mm/s
 _XARM_LIFECYCLE_SPEED_DEG = 20.0
 _XARM_LIFECYCLE_ACCEL_DEG = 500.0
 _XARM6_INITIAL_JOINTS_DEG = [0.0, -40.0, -50.0, 0.0, 90.0, 0.0]
-# TODO (CC): change this once we have 7dof arm setup
-_XARM7_INITIAL_JOINTS_DEG = [0.0, 0.0, 0.0, 0.0, 0.0, -90.0, 0.0]
+_XARM7_INITIAL_JOINTS_DEG = [0.0, 0.0, 0.0, 0.0, 0.0, -90.0, 20.0]
 
 # XArm mode codes
 _XARM_MODE_POSITION = 0
